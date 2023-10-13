38720309943bb8487edcb157fb133e5a3204c040
Lakmini-Bandarigodage <143235230+Lakmini-Bandarigodage@users.noreply.github.com>
2023-10-13 18:36:29 +0530

    Modify once more after Gayani makes updates

---
+++ Number Python.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_square_and_cube(number):
+    square = math.pow(number,2)
+    cube = number ** 3
+    return square, cube
+
+if __name__ == "__main__":
+    try:
+        user_number = float(input("Enter a number: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        exit()
+
+    square_result, cube_result = calculate_square_and_cube(user_number)
+    print(f"The square of {user_number} is: {square_result.....}")
+    print(f"The cube of {user_number} is: {cube_result.....}")
