7b7b1ebf83979c7a16189dc70705b08b6f4adddb
Lakmini-Bandarigodage <143235230+Lakmini-Bandarigodage@users.noreply.github.com>
2023-10-13 12:58:28 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,14 +15,4 @@ def custom_operation(num1, num2, operation_type):
 
     return result
 
-if __name__ == "__main__":
-    try:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
-        operation_type = input("Enter the operation type (add, subtract, multiply, divide): ").lower()
-    except ValueError:
-        print("Invalid input. Please enter valid numbers.")
-        exit()
 
-    operation_result = custom_operation(num1, num2, operation_type)
-    print(f"The result of the custom operation is: {operation_result}")
